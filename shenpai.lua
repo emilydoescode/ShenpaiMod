fee622f207825a1888e6a98f9809a3daea5b57ad
Emily <muffintimeyay@gmail.com>
2025-01-09 22:34:03 -0300

    i am an idiot

---
+++ shenpai.lua
@@ -0,0 +1,12 @@
+----------------------------------------------
+------------MOD CODE -------------------------
+
+
+sendDebugMessage("Launching Negate Texture Pack!", "NegateTexturePack")
+
+SMODS.Atlas{key = "cards_1", path = "8BitDeck.png", px = 71, py = 95, prefix_config = { key = false } }
+SMODS.Atlas{key = "cards_2", path = "8BitDeck.png", px = 71, py = 95, prefix_config = { key = false } }
+SMODS.Atlas{key = "Joker", path = "Jokers.png", px = 71, py = 95, prefix_config = { key = false } }
+
+----------------------------------------------
+------------MOD CODE END----------------------
